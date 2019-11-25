42dc00a052d0f432b53985ad8a8fce7ab043228d
Bharat Makhija <Bharat.Makhija@go-mmt.com>
2019-11-25 15:11:02 +0530

    First commit

---
+++ src/test/java/com/bharat/microservice/apiservice/ApiserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharat.microservice.apiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
